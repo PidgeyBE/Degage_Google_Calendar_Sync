fc9e931f6edf0be2423fa315a7aacfa4c5595755
Pieterjan Soetaert <pieterjan.soetaert@robovision.eu>
2019-09-25 19:01:26 +0200

    Fix renaming bug when trip request got accepted

--- tools/events.py
+++ tools/events.py
@@ -7,14 +7,17 @@ class EventRegistry:
     def update_event(self, start, end, summary, description, google=False, degage=False, id='', origin='google'):
         """ Add or update event to Events """
         uid = f'{start}---{end}'
-        recreate = False
         if uid in self.events:
             self.events[uid]['google'] = self.events[uid]['google'] or google
             self.events[uid]['degage'] = self.events[uid]['degage'] or degage
 
-            # detect if car request got accepted to update status in gcalendar 
+            # detect if car request got accepted to update status in gcalendar
+            # this happens when in google Calendar there is [REG] in the name
+            # while not anymore in degage. Since Google is fetched first, we
+            # can check this case
             if "[REQ]" in self.events[uid]['summary'] and "[REQ]" not in summary:
-                recreate = True
+                self.events[uid]['summary'] = summary
+                self.events[uid]['recreate'] = True
         else:
             self.events[uid] = {
                 'id': id,
@@ -25,7 +28,7 @@ class EventRegistry:
                 'google': google,
                 'degage': degage,
                 'origin': origin,
-                'recreate': recreate
+                'recreate': False
                             }
 
     def update_events(self, events, google=False, degage=False):
